36f877ccad71b8c497b851886a5df30d14fe4cf6
= <aguirremra@gmail.com>
2017-09-15 15:25:46 -0700

    final changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,70 @@
-var heros = ["aqua man", "wonder woman","batman","superman","green lantern","the joker","lex luthor","spiderman","cat woman","poison ivy","captain america","the hulk", "thor"];
-
-renderButtons();
-
-function renderButtons(){
-    $("#heroButtons").empty();
-    for (var i = 0; i < heros.length; i++) {
-      $("<button>")
-            .addClass("hero")
-            .attr("data-name", heros[i])
-            .text(heros[i])            
-            .on("click", function() {
-              // do something
-              displayHeros($(this).data("name"));
-            })
-            .appendTo($("#heroButtons"));
-    }
-}
-
-function displayHeros(hero){
-  $("#hero-gifs").empty();
-    // queryURL for Giphy API
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=80cc2ded30e0457fa2a4fb3ad811786f&limit=10";
-
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).done(function(response) {
-      console.log(response);
-      var results = response.data;
-
-      for (var i = 0; i < results.length; i++) {
-        var heroDiv = $("<div class='hero-images'>");
-        var rating = results[i].rating;
-        var p = $("<p>").text("Rating: " + rating);
-        var heroImage = $("<img>");
-        heroImage
-        .attr("src", results[i].images.fixed_width_still.url)
-        .attr("data-alt", results[i].images.fixed_width.url)
-        .on("click", function(){
-           //getImage($(this).attr("src"));
-           var oldSrc = $(this).attr("src");
-           var oldAlt = $(this).data("alt"); //same as $(this).attr("data-alt");
-           $(this).attr("src", oldAlt);
-           $(this).data("alt", oldSrc);          
-        });        
-        heroDiv
-          .append(p)
-          .append(heroImage)
-        // $("#hero-gifs").prepend(heroDiv);
-          .appendTo($("#hero-gifs"));
-        // $("#hero-gifs")
-        // .append(p)
-        // .append(heroImage);
+//object for giftastic program
+var giftastic = {
+  //array of heros 
+  heros: ["aqua man", "wonder woman","batman","superman","green lantern","the joker","lex luthor","spiderman","cat woman","poison ivy","captain america","the hulk", "thor"],
+  //function to dynamically create buttons
+  renderButtons: function(){
+      $("#heroButtons").empty();
+      for (var i = 0; i < this.heros.length; i++) {
+        $("<button>")
+              .addClass("hero")
+              .attr("data-name", this.heros[i])
+              .text(this.heros[i])            
+              .on("click", function() {
+                // display heros when button is clicked
+                giftastic.displayHeros($(this).data("name"));
+              })
+              .appendTo($("#heroButtons"));
       }
-    });
+  },
+
+  displayHeros: function(hero){
+    $("#hero-gifs").empty();
+      // queryURL for Giphy API
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=80cc2ded30e0457fa2a4fb3ad811786f&limit=10";
+
+      $.ajax({
+        url: queryURL,
+        method: 'GET'
+      }).done(function(response) {
+        console.log(response);
+        var results = response.data;
+        //loop through results
+        for (var i = 0; i < results.length; i++) {
+          var heroDiv = $("<div class='hero-images'>");
+          var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
+          var heroImage = $("<img>");
+          heroImage
+          .attr("src", results[i].images.fixed_height_small_still.url)
+          .attr("data-alt", results[i].images.fixed_height_small.url)
+          //when image is clicked swap gifs
+          .on("click", function(){
+             var oldSrc = $(this).attr("src");
+             var oldAlt = $(this).data("alt"); //same as $(this).attr("data-alt");
+             $(this).attr("src", oldAlt);
+             $(this).data("alt", oldSrc);          
+          });        
+          heroDiv
+            .append(p)
+            .append(heroImage)
+            .appendTo($("#hero-gifs"));
+        }
+      });
+  }
 }
 
+giftastic.renderButtons();
+//click event for adding new hero buttons
 $("#add-hero").on("click", function(event){
+  //prevent default submit action
   event.preventDefault();
-  var hero = $("#hero-input").val().trim();
-  heros.push(hero);
-  renderButtons();
+  var hero = $("#hero-input").val().trim().toLowerCase();
+  //add new item to array
+  giftastic.heros.push(hero);
+  //clears the input box after clicking submit
+  $("form").trigger("reset");
+  //render buttons after adding new item to array
+  giftastic.renderButtons();
 })
 
