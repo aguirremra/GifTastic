88b4ff62c2d44897d67bb468164f7efb058d668a
= <aguirremra@gmail.com>
2017-09-14 20:51:46 -0700

    my changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,16 +8,17 @@ function renderButtons(){
       $("<button>")
             .addClass("hero")
             .attr("data-name", heros[i])
-            .text(heros[i])
+            .text(heros[i])            
+            .on("click", function() {
+              // do something
+              displayHeros($(this).data("name"));
+            })
             .appendTo($("#heroButtons"));
-            // .on("click", function() {
-            //   // do something
-            //   //callSomeFunction();
-            // });
     }
 }
 
 function displayHeros(hero){
+  $("#hero-gifs").empty();
     // queryURL for Giphy API
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=80cc2ded30e0457fa2a4fb3ad811786f&limit=10";
 
@@ -29,24 +30,31 @@ function displayHeros(hero){
       var results = response.data;
 
       for (var i = 0; i < results.length; i++) {
-        var heroDiv = $("<div class='hero'>");
+        var heroDiv = $("<div class='hero-images'>");
         var rating = results[i].rating;
         var p = $("<p>").text("Rating: " + rating);
         var heroImage = $("<img>");
-        heroImage.attr("src", results[i].images.fixed_height.url);
+        heroImage
+        .attr("src", results[i].images.fixed_width_still.url)
+        .attr("data-alt", results[i].images.fixed_width.url)
+        .on("click", function(){
+           //getImage($(this).attr("src"));
+           var oldSrc = $(this).attr("src");
+           var oldAlt = $(this).data("alt"); //same as $(this).attr("data-alt");
+           $(this).attr("src", oldAlt);
+           $(this).data("alt", oldSrc);          
+        });        
         heroDiv
           .append(p)
-          .append(heroImage);
-        $("#hero-gifs").prepend(heroDiv);
+          .append(heroImage)
+        // $("#hero-gifs").prepend(heroDiv);
+          .appendTo($("#hero-gifs"));
+        // $("#hero-gifs")
+        // .append(p)
+        // .append(heroImage);
       }
     });
 }
-
-$("button").on("click", function(){
-    console.log("I'm clicked");
-    var hero = $(this).attr("data-name");
-    displayHeros(hero);
-});
 
 $("#add-hero").on("click", function(event){
   event.preventDefault();
@@ -54,3 +62,4 @@ $("#add-hero").on("click", function(event){
   heros.push(hero);
   renderButtons();
 })
+
